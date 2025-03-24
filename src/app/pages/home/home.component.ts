60068a146d1d21a76fcde16b600c7871f1bed741
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2025-03-24 23:09:39 +0100

    el perfil de usuario

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DatosBasicosComponent } from "./datos-basicos/datos-basicos.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [DatosBasicosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
